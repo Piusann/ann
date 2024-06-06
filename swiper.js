b5300ab0e019145f183e8ebc881d92a78b7017f6
Piusann <piusmaryann77@gmail.com>
2024-06-06 16:23:11 +0100

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,29 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 15,
+    loop:true,
+    grabCursor:true,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView:2,
+
+        },
+        950:{
+            slidesPerView:3,
+        }
+      }
+  });
